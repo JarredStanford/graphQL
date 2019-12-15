a8cbe9a0b3304ff8865162821600915f526d7c21
JarredStanford <stanj89@gmail.com>
2019-12-15 18:18:10 -0500

    mutations/resolvers

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { find, filter } = require('lodash');
 
-const typeDefs = gql`
+import 'dotenv/config';
+
+const schema = gql`
 type Book {
     title: String
-    author: String
-}
+    author: Author
+  }
 
-type Query {
+  type Author {
     books: [Book]
+  }
+
+  type Query {
+    author: Author
+  }
 }`
 
 const books = [
@@ -20,3 +28,28 @@ const books = [
         author: 'Michael Crichton'
     }
 ]
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+
+const resolvers = {
+    Query: {
+        author(parent, args, context, info) {
+            return find(authors, { id: args.id });
+        },
+    },
+    Author: {
+        books(author) {
+            return filter(books, { author: author.name });
+        },
+    },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
